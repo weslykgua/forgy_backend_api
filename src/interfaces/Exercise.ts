5d14cd9042e56984758440ed6306f02ea41dad53
Wesly Cagua <weslykgua@gmail.com>
2026-01-16 16:41:10 -0300

    Base

---
+++ src/interfaces/Exercise.ts
@@ -0,0 +1,11 @@
+export interface Exercise {
+    id: string;
+    name: string;
+    muscle: string;
+    video: string;
+    description: string;
+    difficulty: 'Principiante' | 'Intermedio' | 'Avanzado';
+    equipment: string;
+    instructions: string[];
+    createdAt: string;
+}
