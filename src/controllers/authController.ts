21c3404d75315f20cfa91d5fdb38acec0e0b0e0d
Wesly Cagua <weslykgua@gmail.com>
2026-06-19 03:25:54 -0400

    fix: Ensure JWT_SECRET is required by using non-null assertion in auth-related controllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 import prisma from '../config/database'
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET!
 if (!JWT_SECRET) throw new Error('JWT_SECRET environment variable is required')
 
 interface JWTPayload {
--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET!
 if (!JWT_SECRET) throw new Error('JWT_SECRET environment variable is required')
 
 interface TokenData {
